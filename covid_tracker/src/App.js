8287016876db2490917ee291c6e8a274ecca1f8e
franklin,petra <petrafranklin.gmail.com>
2020-08-15 19:30:35 -0700

    updating and refactoring

--- covid_tracker/src/App.js
+++ covid_tracker/src/App.js
@@ -3,6 +3,7 @@ import{ Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
 import image from "./assets/lineOfMaskWearers.jpg";
+import bannerImage from "./assets/covid-19-icon.png";
 
 class App extends React.Component {
   state = {
@@ -26,7 +27,7 @@ class App extends React.Component {
     const { data, country } = this.state;
     return (
       <div className={styles.container}>
-        <h1 className={styles.header}>Covid Tracker</h1>
+        <img className={styles.header} src={ bannerImage } alt="Covid Banner"/>
         <img className={styles.image} style={{mixBlendMode: "multiply"}} src={image} alt="match_distancing"/>
         
         <Cards data={ data }/>
