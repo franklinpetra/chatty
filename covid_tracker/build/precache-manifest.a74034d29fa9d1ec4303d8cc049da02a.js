e4b4e41509443f40aca669248b279fc3e0842aff
franklin,petra <petrafranklin.gmail.com>
2020-08-15 20:06:56 -0700

    updates for netlify

---
+++ covid_tracker/build/precache-manifest.a74034d29fa9d1ec4303d8cc049da02a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c837194e6e3f45fbd54e3ba794a70bd4",
+    "url": "/covid_tracker/index.html"
+  },
+  {
+    "revision": "e8bc1900473e97e6b187",
+    "url": "/covid_tracker/static/css/main.95b091c2.chunk.css"
+  },
+  {
+    "revision": "c60ccf56ae6d50937e6c",
+    "url": "/covid_tracker/static/js/2.fc1067ef.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid_tracker/static/js/2.fc1067ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8bc1900473e97e6b187",
+    "url": "/covid_tracker/static/js/main.5d5f64ff.chunk.js"
+  },
+  {
+    "revision": "4c5145b2abee34e7044b",
+    "url": "/covid_tracker/static/js/runtime-main.fb184295.js"
+  },
+  {
+    "revision": "25ddb02cd8d184e73bb5a1f8ff97511e",
+    "url": "/covid_tracker/static/media/covid-19-icon.25ddb02c.png"
+  }
+]);
--- covid_tracker/build/service-worker.js
+++ covid_tracker/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f6d66cf4b3280eec0b3c4ce15b1e98eb.js"
+  "/covid_tracker/precache-manifest.a74034d29fa9d1ec4303d8cc049da02a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid_tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
